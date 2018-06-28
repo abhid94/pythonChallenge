caf44bbd4d22c197e3ecc7b7b1b59182f6fcd7b4
Abhi Dutta <abhid94@gmail.com>
2018-06-29 01:20:12 +1000

    Finished q4

--- q4.py
+++ q4.py
@@ -2,14 +2,35 @@
 import urllib.request
 import re
 
+'''
 x = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345')
 data = x.read().decode('utf-8')
 print(data)
 num = re.findall('\d+', data)
 
+
 url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
 url = url + str(num[0])
 print(url)
 
-#loop the above logic 
-#while True :
+z = urllib.request.urlopen(url)
+data2 = z.read().decode('utf-8')
+print(data2)
+
+'''
+
+#loop the above logic
+url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
+num = ['12345']
+pattern = re.compile("and the next nothing is (\d+)")
+
+while True :
+    x = urllib.request.urlopen(url + str(num))
+    data = x.read().decode('utf-8')
+    print(data)
+    match = pattern.search(data)
+    if match == None :
+        break;
+    num = match.group(1)
+
+print("reached the end")
